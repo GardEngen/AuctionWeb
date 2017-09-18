0fec1e5a29ba99ff30e4ea72d99bd7396f1447e0
NikolaiGrieg <raugzok@gmail.com>
2017-09-18 16:08:07 +0200

    Fixed product classes
    Now writes and reads properly from database

---
+++ src/java/boundary/ProductFacade.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boundary;
+
+import entities.Product;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author raugz
+ */
+@Stateless
+public class ProductFacade extends AbstractFacade<Product> {
+
+    @PersistenceContext(unitName = "persistence unit")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProductFacade() {
+        super(Product.class);
+    }
+    
+    public String printProductNames(){
+            String out ="";
+    for(Product a : findAll()){
+           out += a.getName()+ "; ";
+       }
+       return out;
+    }
+    
+}
---
+++ src/java/my/presentation/ProductView.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.presentation;
+
+import boundary.ProductFacade;
+import entities.Product;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author raugz
+ */
+@Named(value = "productView")
+@RequestScoped
+public class ProductView {
+
+    @EJB
+    private ProductFacade productFacade;
+    private Product product;
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    /**
+     * Creates a new instance of ProductView
+     */
+    public ProductView() {
+        this.product = new Product();
+    }
+    
+    public int getNumberOfProducts(){
+        return productFacade.findAll().size();
+    }
+    
+    public String postProduct(){
+        this.productFacade.create(product);
+        return "theend";
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+    
+        public String getAllProducts(){
+        return productFacade.printProductNames();
+    }
+}
